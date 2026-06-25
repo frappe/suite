8bb76a6f020d735a1f214fc14458d157b4d3937d
Bread Genie <msuhailbh07@gmail.com>
2026-06-25 19:05:41 +0530

    fix(meet): use surface-gray-6 token for tile background

--- frontend/src/apps/meet/components/MeetingPreview.vue
+++ frontend/src/apps/meet/components/MeetingPreview.vue
@@ -36,7 +36,7 @@
  
 							<div
 								v-if="!isCameraOn"
-								class="absolute inset-0 bg-[#383838] flex items-center justify-center"
+								class="absolute inset-0 bg-surface-gray-6 flex items-center justify-center"
 							>
 								<div class="text-center text-white">
 									<MeetingAvatar
