42430987dff0b4b13aba9d710c9c24655694a8ef
Dinie Masjuki <diniemasjuki@users.noreply.github.com>
2026-07-03 09:37:07 +0800

    Comment out website redirect for root path

--- suite/hooks.py
+++ suite/hooks.py
@@ -64,7 +64,7 @@ website_route_rules = [
 
 # mail — website redirects
 website_redirects = [
-	{"source": "/", "target": "/suite"},
+	# {"source": "/", "target": "/suite"},
 	{
 		"source": "/auth/validate",
 		"target": "/api/method/suite.mail.api.auth.validate",
