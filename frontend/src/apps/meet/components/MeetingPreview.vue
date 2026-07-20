2e37cb135684f5d41507f7f2ef8ddfbf1947fc2c
Bread Genie <msuhailbh07@gmail.com>
2026-07-20 16:41:20 +0530

    fix(meet): allow scroll in preview page

--- frontend/src/apps/meet/components/MeetingPreview.vue
+++ frontend/src/apps/meet/components/MeetingPreview.vue
@@ -1,5 +1,5 @@
 <template>
- 	<div class="flex-1 flex flex-col bg-surface-base" data-testid="meeting-preview">
+	<div class="flex-1 min-h-0 overflow-y-auto flex flex-col bg-surface-base" data-testid="meeting-preview">
 		<div class="flex-1 flex lg:flex-row flex-col text-ink-gray-8">
  			<div class="max-w-7xl mx-auto w-full flex lg:flex-row flex-col">
  				<!-- Video Section -->
