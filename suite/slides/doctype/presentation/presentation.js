835debeed1121b9d94d5eac7c893fcb37fab5335
Bread Genie <msuhailbh07@gmail.com>
2026-08-27 14:12:58 +0530

    feat(slides): add open presentation link to desk form
    Add web link on Presentation DocType similar to Meet Room's Join Meeting. Shows 'Open Presentation' linking to /slides/presentation/<name>/<slug> when the document is saved.

--- suite/slides/doctype/presentation/presentation.js
+++ suite/slides/doctype/presentation/presentation.js
@@ -3,6 +3,10 @@
 
 frappe.ui.form.on("Presentation", {
 	refresh: function (frm) {
+		if (!frm.doc.__islocal) {
+			const slug = frm.doc.slug ? `/${frm.doc.slug}` : ""
+			frm.add_web_link(`/slides/presentation/${frm.doc.name}${slug}`, __("Open Presentation"))
+		}
 		frm.add_custom_button("Optimize Images", function () {
 			frappe.call({
 				method: "suite.slides.doctype.presentation.presentation.optimize_images",
